569a41f3d17053f650d099c45999ad0170d41a6d
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-08-31 21:10:34 +0800

    Update subtring method to assign number of occurences to hash

--- subtrings.rb
+++ subtrings.rb
@@ -4,13 +4,16 @@ def sub_string(text, arr)
   arr.reduce(Hash.new(0)) do |acc,val|
     # binding.pry
     if text.include?(val)
-      acc[val] += 1
+      occurence = text.scan(val).count
+      acc[val] += occurence
     end
     acc
   end
+
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 print "Enter text: "
 text = gets.chomp
+
 puts sub_string(text.downcase, dictionary)
