d3d70ebc627da7fb6b8a7cbece914e19539fdc15
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-08-31 21:16:39 +0800

    Add more details to comments

--- subtrings.rb
+++ subtrings.rb
@@ -1,5 +1,7 @@
 require "pry-byebug"
 
+#iterate through dict array, assign number of occurences in text to hash if there are any.
+#scan text for current value if it exist. Then uses count to evaluate occurences to integer value.
 def sub_string(text, arr)
   arr.reduce(Hash.new(0)) do |acc,val|
     # binding.pry
