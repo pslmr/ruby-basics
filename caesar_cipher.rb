988d1673a867777039fea0b6be41e16ac1acd4ae
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-08-31 18:15:20 +0800

    Update code to keep original case of the input
    Made changes to char_to_num and shift methods to evaluate both lowercase and uppercase chars. Removed #lowercase and #capitalize method calls to num_arr and puts.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,17 +1,17 @@
 def caesar_cipher(text, to_shift = 5)
   # char_arr = text.downcase.split(//)
   # num_arr = char_to_num(char_arr) 
-  num_arr = char_to_num(text.downcase.chars)
+  num_arr = char_to_num(text.chars)
   shifted_arr = shift(num_arr, to_shift)
-  puts "Caesar cipher: " << num_to_char(shifted_arr).join.capitalize
+  puts "Caesar cipher: " << num_to_char(shifted_arr).join
 
 end
 
-#Converts char values to it's integer ordinal value, reverts changes if value is outside of 97..122
+#Converts char values to it's integer ordinal value, reverts changes if value is outside of 97..122 ** 65..90
 def char_to_num(arr)
   new_arr = arr.map do |x|
       x = x.ord
-      if x < 97 || x > 122
+      if x < 97 && x > 90 || x < 65 || x > 122
         x = x.chr
       end
       x
@@ -32,13 +32,16 @@ def num_to_char(arr)
 
 end
 
-#shifts every value based on given shift factor, ignores shift if value is not a number.
+#shifts every value based on given shift factor, ignores shift if value is not a number. 
+#checks if val represents an uppercase char or lowercase, based on ordinal integer conversion value. Then does the shift based on shift factor 
 def shift(arr, to_shift)
   arr.map do |val|
     if val.class == Integer
       val += to_shift
       if val > 122
         val = 96 + ((122 - val) * -1)
+      elsif val > 90 && val < 97
+        val = 64 + ((90 - val) * -1)
       end
     end
     val
