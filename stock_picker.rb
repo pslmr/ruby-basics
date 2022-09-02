b94665b3b097eb9842a38b1e4d62612bf85c784f
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-09-02 10:35:43 +0800

    Add details to comments

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,24 +1,28 @@
 require "pry-byebug"
 
+
+#For every iteration check if value is less than buy var, if true assign value and index.
+#If diff of value and buy is greater than value of profit assign it and change value of days array.
+#returns days array after all iterations are done
 def stock_picker(arr)
   buy = arr[0]
   buy_idx = 0
   profit = 0
+  days = [0,0]
 
-  arr.each_with_index.reduce(Array.new(2,0)) do |acc, (val, i)|
-    if val < buy
-      buy = val
-      buy_idx = i 
+  arr.each_with_index do |v, i|
+    if v < buy
+      buy = v
+      buy_idx = i
       next
     end
 
-    if val - buy > profit
-      profit = val - buy
-      acc = [buy_idx, i]
+    if v - buy > profit 
+      profit = v - buy
+      days = [buy_idx, i]
     end
-
-    acc
   end
+  days
 
 end
 
