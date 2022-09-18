a8899f593cbd4b6273df9f6ab1884357e7256230
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-09-18 20:23:51 +0800

    Add display_board method to class Game

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,7 +7,27 @@ end
 
 class Game
 
+  def initialize(player)
+      @player = player
+      @board = [
+      [" ", "|", " ", "|", " "],
+      ["-", "+", "-", "+", "-"],
+      [" ", "|", " ", "|", " "],
+      ["-", "+", "-", "+", "-"],
+      [" ", "|", " ", "|", " "]]
+  end
+
+  def display_board
+    @board.each do |row|
+      row.each do |col|
+        print col
+      end
+      print "\n"
+    end
+  end
 end
 
 p1 = Player.new('x')
 p2 = Player.new('o')
+new_game = Game.new("player")
+new_game.display_board
