ff4a5e087ed5ed22c6c5755ae2cbb23e1f9fbb2e
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-09-19 17:15:57 +0800

    Update play method, validate already occupied positions

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -17,16 +17,28 @@ class Game
     while true
       if i.odd?
         display_board
-        puts 'P1 enter position (1-9): '
+        print 'P1 enter position (1-9): '
         p1_choice = gets.chomp
+        while @player2_moves.include?(p1_choice) || @player1_moves.include?(p1_choice)
+          puts 'Position already occupied, choose another position!'
+          print 'P1 enter position (1-9): '
+          p1_choice = gets.chomp
+        end
+        @player1_moves.push(p1_choice)
         p1_pos(p1_choice)
       else
         display_board
-        puts 'P2 enter position (1-9): '
+        print 'P2 enter position (1-9): '
         p2_choice = gets.chomp
+        while @player2_moves.include?(p2_choice) || @player1_moves.include?(p2_choice)
+          puts 'Position already occupied, choose another position!'
+          print 'P2 enter position (1-9): '
+          p2_choice = gets.chomp
+        end
+        @player2_moves.push(p2_choice)
         p2_pos(p2_choice)
       end
-
+      puts "\n"
       i += 1
     end
 
