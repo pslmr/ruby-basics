aec309a3df982aa482c1993d0dcb3bc00f3e3471
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-09-19 18:34:40 +0800

    Add check_winner method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 class Game
   def initialize
     @player1_moves = []
@@ -17,6 +19,7 @@ class Game
     while true
       if i.odd?
         display_board
+        puts "\n"
         print 'P1 enter position (1-9): '
         p1_choice = gets.chomp
         while @player2_moves.include?(p1_choice) || @player1_moves.include?(p1_choice)
@@ -24,10 +27,15 @@ class Game
           print 'P1 enter position (1-9): '
           p1_choice = gets.chomp
         end
-        @player1_moves.push(p1_choice)
+        @player1_moves.push(p1_choice.to_i)
         p1_pos(p1_choice)
+        if check_winner
+          display_board
+          return puts 'P1 wins'
+        end
       else
         display_board
+        puts "\n"
         print 'P2 enter position (1-9): '
         p2_choice = gets.chomp
         while @player2_moves.include?(p2_choice) || @player1_moves.include?(p2_choice)
@@ -35,13 +43,42 @@ class Game
           print 'P2 enter position (1-9): '
           p2_choice = gets.chomp
         end
-        @player2_moves.push(p2_choice)
+        @player2_moves.push(p2_choice.to_i)
         p2_pos(p2_choice)
+        if check_winner
+          display_board
+          return puts 'P2 wins'
+        end
       end
       puts "\n"
       i += 1
     end
 
+  end
+
+  def check_winner
+    winning_patterns = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7]
+    ]
+
+    # binding.pry
+
+    winning_patterns.each do |row|
+      if row.eql?(@player1_moves.sort)
+        return true
+      elsif row.eql?(@player2_moves.sort)
+        return true
+      end
+    end
+
+    false
   end
 
   def p1_pos(choice)
