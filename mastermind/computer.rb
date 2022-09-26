69c18699ac348be99098bdbfa25e4d300b7ad842
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-09-26 20:09:08 +0800

    Add gen_code method

---
+++ mastermind/computer.rb
@@ -0,0 +1,13 @@
+class Computer
+  NUM_POOL = [1,2,3,4,5,6]
+
+  attr_reader :code_combo
+
+  def initialize
+    @code_combo = []
+  end
+
+  def gen_code
+    @code_combo = 4.times.map {Random.rand(6)+1}
+  end
+end
