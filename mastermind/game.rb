40ee9314ef276435e515ef48b90280339d5b99c6
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-09-27 19:04:59 +0800

    Add find_match method

--- mastermind/game.rb
+++ mastermind/game.rb
@@ -8,10 +8,22 @@ class Game
 
   def initialize
     @computer = Computer.new
+    @player = Player.new
   end
 
   def start
     @computer.gen_code
+    @player.get_guess
+    puts find_match
+    p @computer.code_combo
+  end
+
+  def find_match
+    match = ['Match: ']
+    @computer.code_combo.each_with_index do |x, i|
+      match.push('M') if x == @player.code_guess[i]
+    end
+    match.join(' ')
   end
 
 end
